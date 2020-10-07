a431a9066ac51375878d2e25c1ed64477054d3cb
valerio <valerio.deprosperi@gmail.com>
2020-10-07 18:31:40 +0200

    cambio titoli autori  img anno album

--- index.php
+++ index.php
@@ -28,13 +28,49 @@
 
         <div class="disc">
           <div class="poster">
-            <img src="https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg" alt="">
+            <img src="https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg" alt="">
           </div>
           <div class="title">
-            <h2>New Jersey</h2>
+            <h2>Live at Wembley 86</h2>
           </div>
           <div class="artist">
-            <p>Bon Jovi <br> 1988</p>
+            <p>Queen <br> 1992</p>
+          </div>
+        </div>
+
+        <div class="disc">
+          <div class="poster">
+            <img src="https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg" alt="">
+          </div>
+          <div class="title">
+            <h2>Ten's Summoner's Tales</h2>
+          </div>
+          <div class="artist">
+            <p>Sting <br> 1993</p>
+          </div>
+        </div>
+
+        <div class="disc">
+          <div class="poster">
+            <img src="https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg" alt="">
+          </div>
+          <div class="title">
+            <h2>Steve Gadd band</h2>
+          </div>
+          <div class="artist">
+            <p>Steve Gadd band <br> 2018</p>
+          </div>
+        </div>
+
+        <div class="disc">
+          <div class="poster">
+            <img src="https://i.ebayimg.com/images/g/1GwAAOSwFytaKr7l/s-l300.jpg" alt="">
+          </div>
+          <div class="title">
+            <h2>Brave new World</h2>
+          </div>
+          <div class="artist">
+            <p>Iron Maiden <br> 2000</p>
           </div>
         </div>
 
@@ -43,22 +79,10 @@
             <img src="https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg" alt="">
           </div>
           <div class="title">
-            <h2>New Jersey</h2>
+            <h2>One more car, one more rider</h2>
           </div>
           <div class="artist">
-            <p>Bon Jovi <br> 1988</p>
-          </div>
-        </div>
-
-        <div class="disc">
-          <div class="poster">
-            <img src="https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg" alt="">
-          </div>
-          <div class="title">
-            <h2>New Jersey</h2>
-          </div>
-          <div class="artist">
-            <p>Bon Jovi <br> 1988</p>
+            <p>Eric Clapton <br> 2002</p>
           </div>
         </div>
 
