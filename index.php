7fcd7e435f432d32e83ad940e2493ab4b9446a52
valerio <valerio.deprosperi@gmail.com>
2020-10-07 18:19:03 +0200

    prova collegamento db

---
+++ index.php
@@ -0,0 +1,68 @@
+<?php include_once("db.php") ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="dist/app.css">
+    <title></title>
+  </head>
+  <body>
+    <header>
+      <!-- ricorda inserireinserire logo -->
+    </header>
+    <!-- prova collegamento db -->
+    <main>
+      <div class="row">
+        <div class="disc">
+          <div class="poster">
+            <img src="https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg" alt="">
+          </div>
+          <div class="title">
+            <h2>New Jersey</h2>
+          </div>
+          <div class="artist">
+            <p>Bon Jovi <br> 1988</p>
+          </div>
+        </div>
+
+        <div class="disc">
+          <div class="poster">
+            <img src="https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg" alt="">
+          </div>
+          <div class="title">
+            <h2>New Jersey</h2>
+          </div>
+          <div class="artist">
+            <p>Bon Jovi <br> 1988</p>
+          </div>
+        </div>
+
+        <div class="disc">
+          <div class="poster">
+            <img src="https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg" alt="">
+          </div>
+          <div class="title">
+            <h2>New Jersey</h2>
+          </div>
+          <div class="artist">
+            <p>Bon Jovi <br> 1988</p>
+          </div>
+        </div>
+
+        <div class="disc">
+          <div class="poster">
+            <img src="https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg" alt="">
+          </div>
+          <div class="title">
+            <h2>New Jersey</h2>
+          </div>
+          <div class="artist">
+            <p>Bon Jovi <br> 1988</p>
+          </div>
+        </div>
+
+      </div>
+    </main>
+  </body>
+</html>
